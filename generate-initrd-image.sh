1439410e976d6a60ed5f58faac11e129fcc4d31b
Wei Jiang <jiangwei909@gmail.com>
2012-03-09 00:02:13 +0800

    force to add new utils

--- generate-initrd-image.sh
+++ generate-initrd-image.sh
@@ -7,18 +7,19 @@ CMDS="fdisk mkfs.ext4 mkfs mkfs.ext3 lsmod bash mount"
 
 function COPYCMD {
     B=.
-    which $1 &> /dev/null || echo "It is not exist" | retrun 5 
+    which $1 &> /dev/null || echo "$1 is not exist" | return 5 
     DIR=`which $1 | grep -o "/.*"`    
     DIR1=`echo $DIR | sed "s@\(.*\)$1@\1@g"`  
   
     [ -d $B$DIR1 ] || mkdir -p $B$DIR1     
-    [ -e $B$DIR ] || cp $DIR $B$DIR1      
-  
+    echo "cp -f $DIR $B$DIR1"
+    cp -f $DIR $B$DIR1      
+   
     for I in `ldd $DIR | grep -o "/[^[:space:]]*"`
     do  
 	DIR1=`echo $I | sed "s@\(.*\)/[^/]*@\1@g"`  
 	[ -d $B$DIR1 ] || mkdir -p $B$DIR1  
-	[ -e $B$I ] || cp $I $B$DIR1
+	cp -f $I $B$DIR1
     done  
 }
 
