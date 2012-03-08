f476fd29661b1646a6836d6239a6e59f3ca8a047
Jiang Wei <jiangwei@gz.iscas.ac.cn>
2012-03-08 15:19:35 +0800

    more changes

--- conver-iso.sh
+++ conver-iso.sh
@@ -5,27 +5,35 @@ BUILD_TMP_DIR=tmp_build
 CURRENT_DIR=$PWD
 SRC_ISO=$1
 DEST_ISO=starlight.iso
+ROOT_FS_PACKAGE=$CURRENT_DIR/rootfs.tgz
 
 mkdir -p $BUILD_TMP_DIR
 pushd $BUILD_TMP_DIR
 
-mkdir -p loopdir
-mount -o loop $SRC_ISO loopdir 
-mkdir cd
-rsync -a -H --exclude=TRANS.TBL loopdir/ cd
-umount loopdir
+mkdir -p cd/install.386
+
+#kdir -p loopdir
+#ount -o loop $SRC_ISO loopdir 
+#kdir cd
+#sync -a -H --exclude-from=$CURRENT_DIR/exclude-iso.txt loopdir/ cd
+#mount loopdir
+cp -a $CURRENT_DIR/isolinux cd/
 
 # put preseed into initrd
 mkdir irmod
 cd irmod
-gzip -d < ../cd/install.386/initrd.gz | \
+gzip -d < $CURRENT_DIR/boot/initrd.img-2.6.32-5-686 | \
     cpio --extract --verbose --make-directories --no-absolute-filenames
-cp $CURRENT_DIR/my_preseed.cfg preseed.cfg
+#cp $CURRENT_DIR/my_preseed.cfg preseed.cfg
+cp $CURRENT_DIR/boot/init .
 find . | cpio -H newc --create --verbose | \
     gzip -9 > ../cd/install.386/initrd.gz
 
 cd ../
 rm -fr irmod/
+#cp $CURRENT_DIR/boot/initrd.img-2.6.32-5-686 cd/install.386/initrd.gz
+cp $CURRENT_DIR/boot/vmlinuz-2.6.32-5-686 cd/install.386/vmlinuz
+cp $ROOT_FS_PACKAGE cd/rootfs.tgz
 
 # Re-generate md5sum
 cd cd
@@ -41,4 +49,6 @@ mkisofs -o $DEST_ISO -r -J -no-emul-boot -boot-load-size 4 \
 # clean up tmp
 rm -rf $BUILD_TMP_DIR
 
+echo 
 echo "OK"
+echo 
