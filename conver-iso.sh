263e9365b21873df7144f39686194a48e2dc5f33
root <root@debian>
2013-08-15 01:04:47 +0800

    Build from iso file

--- conver-iso.sh
+++ conver-iso.sh
@@ -1,61 +1,71 @@
 #! /bin/bash
 
+# Build a distribution based on Debian
+# By Jiang Wei
+# 2013
+
+VERBOSE=v
+
 CURRENT_DIR=$PWD
 BUILD_TMP_DIR=$CURRENT_DIR/tmp_build
+CD_DIR=${BUILD_TMP_DIR}/cd
 
+SRC_ISO=
 DEST_ISO=starlight.iso
-ROOT_FS_PACKAGE=$CURRENT_DIR/rootfs.tgz
 
-INITRD_VMLINUZ=$CURRENT_DIR/initrd-vmlinuz.tgz
+ORIG_INITRD=$BUILD_TMP_DIR/cd/install.386/initrd.gz
 
-ORIG_INITRD=$BUILD_TMP_DIR/boot/initrd.img
-ORIG_VMLINUZ=$BUILD_TMP_DIR/boot/vmlinuz
+PRESEED_FILE=${CURRENT_DIR}/my_preseed.cfg
 
+# Create a temporery directory for build
 mkdir -p $BUILD_TMP_DIR
+mkdir -p ${CD_DIR}
+
 pushd $BUILD_TMP_DIR
 
-mkdir -p boot
-tar -zxf $INITRD_VMLINUZ -C boot
+# Mount iso and sync to a new directory
+mkdir -p loopdir
+if [ -e "${SRC_ISO}" ]
+then
+    mount -o loop loopdir
+else
+    mount /dev/sr0 loopdir 
+fi
 
-mkdir -p cd/install.386
+rsync -a -H --exclude-from=$CURRENT_DIR/exclude-iso.txt loopdir/ ${CD_DIR}
+umount loopdir
 
-#kdir -p loopdir
-#ount -o loop $SRC_ISO loopdir 
-#kdir cd
-#sync -a -H --exclude-from=$CURRENT_DIR/exclude-iso.txt loopdir/ cd
-#mount loopdir
-cp -a $CURRENT_DIR/isolinux cd/
+# Use our isolinux
+cp -a $CURRENT_DIR/isolinux ${CD_DIR}
 
-# put preseed into initrd
+# Put preseed into initrd and re-package
 mkdir irmod
 cd irmod
 gzip -d < $ORIG_INITRD | \
     cpio --extract  --make-directories --no-absolute-filenames
 
-cp $CURRENT_DIR/boot/init .
-find . | cpio -H newc --create --verbose | \
+cp ${PRESEED_FILE} ./preseed.cfg
+
+find . | cpio -H newc --create -${VERBOSE} | \
     gzip -9 > ../cd/install.386/initrd.gz
 
 cd ../
 rm -fr irmod/
-#cp $CURRENT_DIR/boot/initrd.img-2.6.32-5-686 cd/install.386/initrd.gz
-cp $ORIG_VMLINUZ cd/install.386/vmlinuz
-cp $ROOT_FS_PACKAGE cd/rootfs.tgz
 
 # Re-generate md5sum
-cd cd
+cd ${CD_DIR}
 md5sum `find -follow -type f` > md5sum.txt
 cd ..
 
 popd
 
 # Build a new ISO
-mkisofs -o $DEST_ISO -r -J -no-emul-boot -boot-load-size 4 \
-    -boot-info-table -b isolinux/isolinux.bin -c isolinux/boot.cat $BUILD_TMP_DIR/cd
+genisoimage -o $DEST_ISO -r -J -no-emul-boot -boot-load-size 4 \
+    -boot-info-table -b isolinux/isolinux.bin -c isolinux/boot.cat ${CD_DIR}
 
 # clean up tmp
 rm -rf $BUILD_TMP_DIR
 
 echo 
 echo "OK"
-echo 
+echo .....
